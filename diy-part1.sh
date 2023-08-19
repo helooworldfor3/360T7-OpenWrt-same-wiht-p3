7e5f0486549795d13402713c1b0359182ce38fc9
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-19 23:54:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git netgear https://github.com/tracemouse/luci-theme-netgear' >>feeds.conf.default
+echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird' >>feeds.conf.default
