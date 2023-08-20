3c619eb8e94064df5dacded8ed818824720c0d1d
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-20 23:16:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
-git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
